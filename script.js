f95517213fadf9a5d70f5bbf4e9a484286153826
anacgms <acgomes43@gmail.com>
2022-06-22 20:38:45 -0300

    desafio2

---
+++ script.js
@@ -0,0 +1,36 @@
+let idade = 18
+if(idade >= 18){
+  console.log("Você pode ser preso")
+}else{
+    console.log("Você pode aprontar")
+    }
+
+    let human = false
+if(idade >= 18 && human === true){
+    console.log("Você é um humano normal")
+}else{
+     console.log("Você é uma criatura mística")
+}
+
+let mes = "Julho"
+
+if(mes === "Janeiro" || mes === "Dezembro"){
+     console.log("Feliz aniverssauro!")
+}else{
+     console.log("Não é seu aniversário, Rômulo!")
+}
+
+let nome = "Mariana"
+if(nome[0] === "R"){
+     console.log("Que nome elegante!")
+}else{
+     console.log("Não tão elegante!")
+}
+
+let sobrenome = "Feijão"
+
+if(sobrenome.length >= 6 || nome[0] === "E"){
+     console.log("Seu nome é top")
+}else{
+     console.log("Sinto muito, seu nome não é top!")
+}
